953a2804cdc55214555df531cac824e209ee6e68
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-05-23 03:35:48 +0000

    Implement fail method

--- include/main/cpp/FailureReporterTemplate.hpp
+++ include/main/cpp/FailureReporterTemplate.hpp
@@ -1,8 +1,16 @@
 #ifndef FAILURE_REPORTER_TEMPLATE_HPP
 #define FAILURE_REPORTER_TEMPLATE_HPP
 
+#include <string>
+
+#include "gtest/gtest.h"
+
 #define DECLARE_FAILURE_REPORTER_CLASS(className) \
 class className { \
+public: \
+    virtual void fail(std::string errorMessage) { \
+        FAIL() << errorMessage; \
+    } \
 }
 
 #endif
