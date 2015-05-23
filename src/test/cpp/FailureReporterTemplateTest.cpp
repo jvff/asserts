c9b655e4494bece7ececa28c714ba3bb0bae947b
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-05-23 03:33:13 +0000

    Test for fail method in FailureReporterTemplate
    - It should use the FAIL macro to report the failure message

--- src/test/cpp/FailureReporterTemplateTest.cpp
+++ src/test/cpp/FailureReporterTemplateTest.cpp
@@ -1,4 +1,15 @@
+#include <sstream>
+#include <string>
+
 #include "gtest/gtest.h"
+
+static std::stringstream* failStream;
+
+#ifdef FAIL
+#undef FAIL
+#endif
+
+#define FAIL() *failStream << ""
 
 #include "FailureReporterTemplate.hpp"
 
@@ -10,6 +21,22 @@ TEST(FailureReporterTemplateTest, templateMakesAClass) {
     reporter = new FakeFailureReporter();
 
     EXPECT_TRUE(reporter != NULL);
+
+    delete reporter;
+}
+
+TEST(FailureReporterTemplateTest, failMethod) {
+    std::string errorMessage = "Test error message";
+    FakeFailureReporter *reporter;
+    std::stringstream stream;
+
+    failStream = &stream;
+
+    reporter = new FakeFailureReporter();
+
+    reporter->fail(errorMessage);
+
+    EXPECT_EQ(errorMessage, stream.str());
 
     delete reporter;
 }
