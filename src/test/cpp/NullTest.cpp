8872239138036e55def633264431c8b4c4056996
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-05-27 23:53:29 +0000

    Test if NULL pointer test fails

--- src/test/cpp/NullTest.cpp
+++ src/test/cpp/NullTest.cpp
@@ -13,3 +13,15 @@ TEST(NullTest, succeed) {
 
     EXPECT_TRUE(FakeTestReporter::succeeded);
 }
+
+TEST(NullTest, fail) {
+    const int dummyVariable = 0;
+    const void* pointer = (const void*)&dummyVariable;
+
+    FakeTestReporter::reset();
+
+    assertThat(pointer).isNull();
+
+    EXPECT_TRUE(FakeTestReporter::failed);
+    EXPECT_EQ("Pointer should be NULL", FakeTestReporter::failureMessage.get());
+}
