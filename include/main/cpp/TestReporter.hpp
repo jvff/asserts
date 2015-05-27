67c0d2b4700c6884880d2403451ddcd81d77f430
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-05-27 15:22:01 +0000

    Fail if no supported test library is detected

--- include/main/cpp/TestReporter.hpp
+++ include/main/cpp/TestReporter.hpp
@@ -5,6 +5,8 @@
     typedef TEST_REPORTER TestReporter;
 #elif defined GTEST_INCLUDE_GTEST_GTEST_H_
     #include "GoogleTestReporter.hpp"
+#else
+    #error "No supported test library detected"
 #endif
 
 #endif
