5877683de3f5b24393a031c6c8669085dd1e324a
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-06-01 17:43:31 +0000

    Test if isClassOrStruct works with structs

--- src/test/cpp/ClassTest.cpp
+++ src/test/cpp/ClassTest.cpp
@@ -1,5 +1,6 @@
 #include "gtest/gtest.h"
 
+#include "DummyTypes.hpp"
 #include "FakeTestReporter.hpp"
 
 #include "asserts.hpp"
@@ -11,6 +12,14 @@ TEST(ClassTest, isClassOrStructSucceedsWithClass) {
     FakeTestReporter::reset();
 
     AssertThat<DummyClass>::isClassOrStruct();
+
+    EXPECT_TRUE(FakeTestReporter::succeeded);
+}
+
+TEST(ClassTest, isClassOrStructSucceedsWithStruct) {
+    FakeTestReporter::reset();
+
+    AssertThat<DummyType>::isClassOrStruct();
 
     EXPECT_TRUE(FakeTestReporter::succeeded);
 }
