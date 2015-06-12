9b01fc19dd831217a9a1653f4fa1abaa5a9f6021
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-06-12 12:16:20 -0300

    Move AssertThat class to a separate header

---
+++ include/main/cpp/AssertThat.hpp
@@ -0,0 +1,53 @@
+#ifndef ASSERT_THAT_HPP
+#define ASSERT_THAT_HPP
+
+#include <string>
+
+#include "TestReporter.hpp"
+
+template<typename T>
+class AssertThat {
+private:
+    static const std::string classShouldHaveVirtualDestructorMessage;
+    static const std::string pointerShouldBeNullMessage;
+    static const std::string pointerShouldntBeNullMessage;
+    static const std::string typeShouldBeClassOrStructMessage;
+
+public:
+    static void isClassOrStruct() {
+        test(std::is_class<T>::value, typeShouldBeClassOrStructMessage);
+    }
+
+    static void hasVirtualDestructor() {
+        test(std::has_virtual_destructor<T>::value,
+                classShouldHaveVirtualDestructorMessage);
+    }
+
+private:
+    static void test(bool result, const std::string& failureMessage) {
+        if (result == true)
+            TestReporter::succeed();
+        else
+            TestReporter::fail(failureMessage);
+    }
+
+public:
+    const T& subject;
+
+public:
+    AssertThat(const T& reference) : subject(reference) {
+    }
+
+    AssertThat(AssertThat<T>&) = delete;
+    AssertThat(AssertThat<T>&&) = delete;
+
+    void isNull() {
+        test(subject == NULL, pointerShouldBeNullMessage);
+    }
+
+    void isNotNull() {
+	test(subject != NULL, pointerShouldntBeNullMessage);
+    }
+};
+
+#endif
