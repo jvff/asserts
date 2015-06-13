8406578ba64dcea7e2852ac7ff7f07ba85e0daf7
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-06-13 19:51:44 -0300

    Test if outputting TypeOf<int> to stream works

--- src/test/cpp/TypeOfTest.cpp
+++ src/test/cpp/TypeOfTest.cpp
@@ -1,6 +1,7 @@
 #ifndef TYPE_OF_TEST_HPP
 #define TYPE_OF_TEST_HPP
 
+#include <sstream>
 #include <type_traits>
 
 #include "gtest/gtest.h"
@@ -11,6 +12,14 @@
 
 TEST(TypeOfTest, classTemplateExists) {
     EXPECT_TRUE((std::is_class<TypeOf<DummyType> >::value));
+}
+
+TEST(TypeOfTest, outputStreamOperatorForInt) {
+    std::stringstream stream;
+
+    stream << TypeOf<int>();
+
+    EXPECT_EQ("int", stream.str());
 }
 
 #endif
