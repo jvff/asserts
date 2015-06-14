b93fe7900b350ac5c3f48392f16de3f99c25f9e6
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-06-14 14:24:43 -0300

    Test TypeOf stream operator with primitive types

--- src/test/cpp/TypeOfTest.cpp
+++ src/test/cpp/TypeOfTest.cpp
@@ -23,6 +23,19 @@ TEST(TypeOfTest, outputStreamOperatorFor##TypeName) { \
     EXPECT_EQ(#Type, stream.str()); \
 }
 
+TEST_TYPE_OF(Bool, bool)
+TEST_TYPE_OF(Char, char)
+TEST_TYPE_OF(ShortInt, short)
 TEST_TYPE_OF(Int, int)
+TEST_TYPE_OF(LongInt, long)
+TEST_TYPE_OF(LongLongInt, long long)
+TEST_TYPE_OF(UnsignedChar, unsigned char)
+TEST_TYPE_OF(UnsignedShortInt, unsigned short)
+TEST_TYPE_OF(UnsignedInt, unsigned int)
+TEST_TYPE_OF(UnsignedLongInt, unsigned long)
+TEST_TYPE_OF(UnsignedLongLongInt, unsigned long long)
+TEST_TYPE_OF(Float, float)
+TEST_TYPE_OF(Double, double)
+TEST_TYPE_OF(LongDouble, long double)
 
 #endif
