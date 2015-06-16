d99c50b5f93a8de01cadc9b0799944bd3bc9ac40
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-06-16 14:50:59 -0300

    Test isTheSame with some primitive types

--- src/test/cpp/SameTypeTest.cpp
+++ src/test/cpp/SameTypeTest.cpp
@@ -12,5 +12,17 @@ TYPE_ASSERTION_TEST_T(SameTypeTest, isTheSameAs, 2) {
     this->checkResult("%s should be the same type as %s", TypeOf<TypeParam2>());
 }
 
-SHOULD_SUCCEED(SameTypeTest, isTheSameAs, tuple<bool, bool>);
-SHOULD_FAIL(SameTypeTest, isTheSameAs, tuple<bool, int>);
+SHOULD_SUCCEED(SameTypeTest, isTheSameAs,
+        tuple<bool, bool>,
+        tuple<char, char>,
+        tuple<signed char, signed char>,
+        tuple<unsigned char, unsigned char>,
+        tuple<short, signed short int>,
+        tuple<unsigned long, unsigned long int>);
+SHOULD_FAIL(SameTypeTest, isTheSameAs,
+        tuple<bool, int>,
+        tuple<char, unsigned char>,
+        tuple<char, signed char>,
+        tuple<signed char, char>,
+        tuple<unsigned char, char>,
+        tuple<unsigned int, int>);
