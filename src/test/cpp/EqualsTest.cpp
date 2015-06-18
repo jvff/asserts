a7b6571d23f806ebad05b4af0c24609e51e5fd35
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-06-18 11:18:04 -0300

    Test isEqualTo assertion with more values

--- src/test/cpp/EqualsTest.cpp
+++ src/test/cpp/EqualsTest.cpp
@@ -10,5 +10,24 @@ VALUE_ASSERTION_TEST(EqualsTest, isEqualTo) {
             std::get<1>(parameter), std::get<0>(parameter));
 }
 
-VALUES_SHOULD_SUCCEED(EqualsTest, isEqualTo, std::make_tuple(3, 3));
-VALUES_SHOULD_FAIL(EqualsTest, isEqualTo, std::make_tuple(3, 4));
+VALUES_SHOULD_SUCCEED(EqualsTest, isEqualTo,
+        std::make_tuple(true, true),
+        std::make_tuple(false, false),
+        std::make_tuple(3, 3),
+        std::make_tuple(-192, -192),
+        std::make_tuple(0.125, 0.125),
+        std::make_tuple("bla", "bla"),
+        std::make_tuple(std::string("STRING"), "STRING"));
+VALUES_SHOULD_FAIL(EqualsTest, isEqualTo,
+        std::make_tuple(true, false),
+        std::make_tuple(false, true),
+        std::make_tuple(3, 4),
+        std::make_tuple(192, -192),
+        std::make_tuple(-192, 192),
+        std::make_tuple(-0.125, 0.125),
+        std::make_tuple(0.125, -0.125),
+        std::make_tuple(0.125, 0.25),
+        std::make_tuple(0.25, 0.125),
+        std::make_tuple(0.125, 0.1251),
+        std::make_tuple(0.1251, 0.125),
+        std::make_tuple("bla", "iha"));
