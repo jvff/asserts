24203b46d0d0980c6bb34ee49b96d797185485c8
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-06-25 20:57:50 -0300

    Test if unindexed failure message is empty

--- src/test/cpp/FailureMessageTest.cpp
+++ src/test/cpp/FailureMessageTest.cpp
@@ -5,3 +5,7 @@
 TEST(FailureMessageTest, structTemplateExists) {
     EXPECT_TRUE((std::is_class<FailureMessage<0> >::value));
 }
+
+TEST(FailureMessageTest, invalidFailureMessageIsEmpty) {
+    EXPECT_EQ("", FailureMessage<-1>::message);
+}
