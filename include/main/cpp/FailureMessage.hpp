6fe43a6dfef75080412d52a902141373f9d4db09
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-06-25 20:58:25 -0300

    Add static message field to FailureMessage

--- include/main/cpp/FailureMessage.hpp
+++ include/main/cpp/FailureMessage.hpp
@@ -3,6 +3,10 @@
 
 template <int messageId>
 struct FailureMessage {
+    static const std::string message;
 };
+
+template <int messageId>
+const std::string FailureMessage<messageId>::message = "";
 
 #endif
