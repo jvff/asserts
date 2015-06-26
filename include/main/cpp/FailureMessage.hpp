880898566f9b4218de2ef593da176ee06e6b49b4
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-06-25 21:08:27 -0300

    Create isNull failure message

--- include/main/cpp/FailureMessage.hpp
+++ include/main/cpp/FailureMessage.hpp
@@ -9,4 +9,14 @@ struct FailureMessage {
 template <int messageId>
 const std::string FailureMessage<messageId>::message = "";
 
+const int isNullFailureMessageId = 1;
+
+template <>
+struct FailureMessage<isNullFailureMessageId> {
+    static const std::string message;
+};
+
+const std::string FailureMessage<isNullFailureMessageId>::message =
+        "Pointer should be NULL";
+
 #endif
