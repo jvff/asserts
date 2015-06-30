97b49faf08bccceca462cdcc87a62fb974613845
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-06-30 00:34:16 -0300

    Refactor FailureMessage to use macros
    - Create macros to reduce repeated code

--- include/main/cpp/FailureMessage.hpp
+++ include/main/cpp/FailureMessage.hpp
@@ -1,22 +1,40 @@
 #ifndef FAILURE_MESSAGE_HPP
 #define FAILURE_MESSAGE_HPP
 
-template <int messageId>
-struct FailureMessage {
-    static const std::string message;
-};
+#define DECLARE_FAILURE_MESSAGE_STRUCT_TEMPLATE() \
+    template <int messageId> \
+    DECLARE_FAILURE_MESSAGE_STRUCT()
 
-template <int messageId>
-const std::string FailureMessage<messageId>::message = "";
+#define DECLARE_FAILURE_MESSAGE_STRUCT_TEMPLATE_SPECIALIZATION(MessageId) \
+    template <> \
+    DECLARE_FAILURE_MESSAGE_STRUCT(<MessageId>)
 
-const int isNullFailureMessageId = 1;
+#define DECLARE_FAILURE_MESSAGE_STRUCT(Specialization) \
+    struct FailureMessage Specialization { \
+        static const std::string message; \
+    }
 
-template <>
-struct FailureMessage<isNullFailureMessageId> {
-    static const std::string message;
-};
+#define DEFINE_MESSAGE_ID(MessageId) \
+    const int MessageId = __COUNTER__
 
-const std::string FailureMessage<isNullFailureMessageId>::message =
-        "Pointer should be NULL";
+#define DEFINE_MESSAGE_TEMPLATE(Message) \
+    template <int messageId> \
+    const std::string FailureMessage<messageId>::message = Message
+
+#define DEFINE_MESSAGE(MessageId, Message) \
+    const std::string FailureMessage<MessageId>::message = Message
+
+#define MAKE_MESSAGE(MessageId, Message) \
+    DEFINE_MESSAGE_ID(MessageId); \
+    DECLARE_FAILURE_MESSAGE_STRUCT_TEMPLATE_SPECIALIZATION(MessageId); \
+    DEFINE_MESSAGE(MessageId, Message)
+
+#define MSG(MessageName, Message) \
+    MAKE_MESSAGE(MessageName##FailureMessageId, Message)
+
+DECLARE_FAILURE_MESSAGE_STRUCT_TEMPLATE();
+DEFINE_MESSAGE_TEMPLATE("");
+
+MSG(isNull, "Pointer should be NULL");
 
 #endif
