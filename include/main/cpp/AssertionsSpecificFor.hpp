ae49a8e6b7b55a96e10649376ed014495a485c2c
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-06-30 09:46:03 -0300

    Make AssertionsSpeceficFor extend CommonAssertions

--- include/main/cpp/AssertionsSpecificFor.hpp
+++ include/main/cpp/AssertionsSpecificFor.hpp
@@ -1,8 +1,10 @@
 #ifndef ASSERTIONS_SPECIFIC_FOR_HPP
 #define ASSERTIONS_SPECIFIC_FOR_HPP
 
+#include "CommonAssertions.hpp"
+
 template <typename T>
-class AssertionsSpecificFor {
+class AssertionsSpecificFor : public CommonAssertions {
 };
 
 #endif
