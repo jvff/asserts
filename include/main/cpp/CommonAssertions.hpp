3fae5829b9168041ecdc697a8e2f9d14aa13ce64
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-06-30 16:19:08 -0300

    Create initial CommonAssertions::test method

--- include/main/cpp/CommonAssertions.hpp
+++ include/main/cpp/CommonAssertions.hpp
@@ -1,7 +1,15 @@
 #ifndef COMMON_ASSERTIONS_HPP
 #define COMMON_ASSERTIONS_HPP
 
+#include <string>
+
+#include "TestReporter.hpp"
+
 class CommonAssertions {
+protected:
+    static void test(bool, const std::string&) {
+	TestReporter::succeed();
+    }
 };
 
 #endif
