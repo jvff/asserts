27a48cbc643cd47e894ecec7be99184e54cabab3
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-07-02 16:25:18 -0300

    Implement simple isConstructibleWith assertion
    - No parameters are used yet

--- include/main/cpp/AssertThat.hpp
+++ include/main/cpp/AssertThat.hpp
@@ -17,6 +17,7 @@ class AssertThat : public AssertionsSpecificFor<T> {
 public:
     TYPE_ASSERTION(hasVirtualDestructor, std::has_virtual_destructor<T>::value)
     TYPE_ASSERTION(isClassOrStruct, std::is_class<T>::value)
+    TYPE_ASSERTION(isConstructibleWith, std::is_constructible<T>::value)
 
     TYPE_ASSERTION_WITH_PARAM(isSubClass, std::is_base_of<T2, T>::value)
     TYPE_ASSERTION_WITH_PARAM(isTheSame, std::is_same<T, T2>::value)
