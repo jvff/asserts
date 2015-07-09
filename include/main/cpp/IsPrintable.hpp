10e886f065540c07e33f402107ab3e80696720ae
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-07-09 17:13:07 -0300

    Implement IsPrintable type trait

--- include/main/cpp/IsPrintable.hpp
+++ include/main/cpp/IsPrintable.hpp
@@ -1,9 +1,20 @@
 #ifndef IS_PRINTABLE_HPP
 #define IS_PRINTABLE_HPP
 
+#include <iostream>
+
 template <typename T>
 struct IsPrintable {
-    static const bool value = true;
+    typedef char (&yes)[1];
+    typedef char (&no)[2];
+
+    template <typename U>
+    static yes test(U* u, std::ostream* stream, decltype(&(*stream << *u)));
+
+    template <typename U>
+    static no test(...);
+
+    static const bool value = sizeof(test<T>(0, 0, 0)) == sizeof(yes);
 };
 
 #endif
