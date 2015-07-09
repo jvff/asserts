91ccc2a94dbab2cbbe633df8d88c95fa1a23f1c6
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-07-09 17:12:15 -0300

    Test IsPrintable trait with unprintable class

--- src/test/cpp/IsPrintableTest.cpp
+++ src/test/cpp/IsPrintableTest.cpp
@@ -2,6 +2,12 @@
 
 #include "IsPrintable.hpp"
 
+#include "IsPrintableTest.hpp"
+
 TEST(IsPrintableTest, boolIsPrintable) {
     EXPECT_TRUE((IsPrintable<bool>::value));
+}
+
+TEST(IsPrintableTest, unprintableClassIsntPrintable) {
+    EXPECT_FALSE((IsPrintable<UnprintableClass>::value));
 }
---
+++ src/test/cpp/IsPrintableTest.hpp
@@ -0,0 +1,7 @@
+#ifndef IS_PRINTABLE_TEST_HPP
+#define IS_PRINTABLE_TEST_HPP
+
+class UnprintableClass {
+};
+
+#endif
