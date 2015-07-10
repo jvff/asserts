fbe63308bb0efaf38b95bc9e157707c679ec704d
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-07-10 12:31:17 -0300

    Implement PrintableClass output stream operator
    - To be used for testing

---
+++ src/test/cpp/PrintableClass.cpp
@@ -0,0 +1,6 @@
+#include "PrintableClass.hpp"
+
+std::ostream& operator<<(std::ostream& outputStream,
+	const PrintableClass& instance) {
+    return outputStream << "PrintableClass@" << (void*)&instance;
+}
