f348b5fd29c93bad21bab942c43b91db25443124
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-07-10 14:16:27 -0300

    Refactor to create test fixture for FormatterTest
    - Create helper methods to reduce repeated code

--- src/test/cpp/FormatterTest.cpp
+++ src/test/cpp/FormatterTest.cpp
@@ -1,85 +1,55 @@
 #include <type_traits>
 
-#include <gtest/gtest.h>
-
-#include "Formatter.hpp"
-
 #include "DummyTypes.hpp"
+#include "FormatterTest.hpp"
 #include "PrintableClass.hpp"
 #include "UnprintableClass.hpp"
 
-TEST(FormatterTest, classTemplateExists) {
+TEST_F(FormatterTest, classTemplateExists) {
     EXPECT_TRUE((std::is_class<Formatter<DummyType> >::value));
 }
 
-TEST(FormatterTest, isConstructibleWithParameter) {
+TEST_F(FormatterTest, isConstructibleWithParameter) {
     typedef Formatter<DummyType> formatter;
     typedef DummyType parameter;
 
     EXPECT_TRUE((std::is_constructible<formatter, parameter>::value));
 }
 
-TEST(FormatterTest, formatterIsPrintable) {
-    int value = 2;
-
-    Formatter<int> formatter(value);
-    std::stringstream expected, actual;
-
-    expected << value;
-    actual << formatter;
-
-    EXPECT_EQ(expected.str(), actual.str());
+TEST_F(FormatterTest, formatterIsPrintable) {
+    testFormatter(2);
 }
 
-TEST(FormatterTest, formatFunctionReturnsFormatter) {
+TEST_F(FormatterTest, formatFunctionReturnsFormatter) {
     typedef Formatter<DummyType> expected;
     typedef decltype(format(DummyType())) actual;
 
     EXPECT_TRUE((std::is_same<expected, actual>::value));
 }
 
-TEST(FormatterTest, formatValueOne) {
-    std::stringstream stream;
-
-    stream << format(1);
-
-    EXPECT_EQ("1", stream.str());
+TEST_F(FormatterTest, formatValueOne) {
+    testFormat(1);
 }
 
-TEST(FormatterTest, formatTrueValue) {
-    std::stringstream stream;
-
-    stream << format(true);
-
-    EXPECT_EQ("true", stream.str());
+TEST_F(FormatterTest, formatTrueValue) {
+    testFormat(true, "true");
 }
 
-TEST(FormatterTest, formatFalseValue) {
-    std::stringstream stream;
-
-    stream << format(false);
-
-    EXPECT_EQ("false", stream.str());
+TEST_F(FormatterTest, formatFalseValue) {
+    testFormat(false, "false");
 }
 
-TEST(FormatterTest, formatPrintableClass) {
+TEST_F(FormatterTest, formatPrintableClass) {
     PrintableClass printableObject;
-    std::stringstream expected;
-    std::stringstream actual;
 
-    expected << "PrintableClass@" << (void*)&printableObject;
-    actual << format(printableObject);
-
-    EXPECT_EQ(expected.str(), actual.str());
+    testFormat(printableObject);
 }
 
-TEST(FormatterTest,formatUnprintableClass) {
+TEST_F(FormatterTest,formatUnprintableClass) {
     UnprintableClass unprintableObject;
     std::stringstream expected;
-    std::stringstream actual;
 
     expected << "?(UnprintableClass@" << (void*)&unprintableObject << ")";
-    actual << format(unprintableObject);
 
-    EXPECT_EQ(expected.str(), actual.str());
+    testFormat(unprintableObject, expected.str());
 }
---
+++ src/test/cpp/FormatterTest.hpp
@@ -0,0 +1,49 @@
+#ifndef FORMATTER_TEST_HPP
+#define FORMATTER_TEST_HPP
+
+#include <sstream>
+#include <string>
+
+#include <gtest/gtest.h>
+
+#include "Formatter.hpp"
+
+class FormatterTest : public testing::Test {
+protected:
+    template <typename T>
+    std::string streamStringValueOf(const T& value) {
+	std::stringstream stream;
+
+	stream << value;
+
+	return stream.str();
+    }
+
+    template <typename T>
+    void testFormatter(const Formatter<T>& formatter,
+	    const std::string& expectedResult) {
+	EXPECT_EQ(expectedResult, streamStringValueOf(formatter));
+    }
+
+    template <typename T>
+    void testFormatter(const Formatter<T>& formatter, const T& value) {
+	testFormatter(formatter, streamStringValueOf(value));
+    }
+
+    template <typename T>
+    void testFormatter(const T& value) {
+	testFormatter(Formatter<T>(value), value);
+    }
+
+    template <typename T>
+    void testFormat(const T& value, const std::string& expectedResult) {
+	testFormatter(format(value), expectedResult);
+    }
+
+    template <typename T>
+    void testFormat(const T& value) {
+	testFormat(format(value), streamStringValueOf(value));
+    }
+};
+
+#endif
