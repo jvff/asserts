d77c21c95e148d934ef8a63536f4f94979f5221d
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-07-09 22:38:17 -0300

    Print value passed to Formatter
    - Store the value in a private field
    - Print the value in the output stream operator function
    - Make the class be friends with the function so that it can access the
      value

--- include/main/cpp/Formatter.hpp
+++ include/main/cpp/Formatter.hpp
@@ -2,15 +2,26 @@
 #define FORMATTER_HPP
 
 template <typename T>
+class Formatter;
+
+template <typename T>
+std::ostream& operator<<(std::ostream&, const Formatter<T>&);
+
+template <typename T>
 class Formatter {
+    friend std::ostream& operator<< <T>(std::ostream&, const Formatter<T>&);
+
+private:
+    const T& value;
+
 public:
-    Formatter(const T&) {
+    Formatter(const T& parameter) : value(parameter) {
     }
 };
 
 template <typename T>
-std::ostream& operator<<(std::ostream& stream, const Formatter<T>&) {
-    stream << 2;
+std::ostream& operator<<(std::ostream& stream, const Formatter<T>& formatter) {
+    stream << formatter.value;
 
     return stream;
 }
