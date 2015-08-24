ffc645b7ac7189159de92b1e1eee650ef2179881
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-08-24 19:52:25 -0300

    Test test method with failure message parameters

--- src/test/cpp/CommonAssertionsTest.cpp
+++ src/test/cpp/CommonAssertionsTest.cpp
@@ -26,3 +26,14 @@ TEST_F(CommonAssertionsTest, testMethodFailsWithParameter) {
 
     shouldFail("Fake test failure message with parameter (86)");
 }
+
+TEST_F(CommonAssertionsTest, testMethodFailsWithParameters) {
+    int firstParameter = 86;
+    char secondParameter = 'a';
+    const std::string message =
+            "Fake test failure message with parameter (%s, %s)";
+
+    FakeCommonAssertions::test(false, message, firstParameter, secondParameter);
+
+    shouldFail("Fake test failure message with parameter (86, a)");
+}
