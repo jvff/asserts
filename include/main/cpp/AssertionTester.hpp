bb464319a3cef1b089bfddb91dba1afe770a1fb2
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-08-25 11:12:53 -0300

    Copy test methods from CommonAssertions

--- include/main/cpp/AssertionTester.hpp
+++ include/main/cpp/AssertionTester.hpp
@@ -1,7 +1,43 @@
 #ifndef ASSERTION_TESTER_HPP
 #define ASSERTION_TESTER_HPP
 
+#include <string>
+
+#include <boost/format.hpp>
+
+#include "Formatter.hpp"
+#include "TestReporter.hpp"
+
 class AssertionTester {
+public:
+    static void test(bool result, const std::string& failureMessage) {
+        if (result == true)
+            TestReporter::succeed();
+        else
+            TestReporter::fail(failureMessage);
+    }
+
+    template <typename... ParameterTypes>
+    static void test(bool result, const std::string& failureMessage,
+            const ParameterTypes&... messageParameters) {
+        if (result == true)
+            TestReporter::succeed();
+        else
+            fail(boost::format(failureMessage), messageParameters...);
+    }
+
+private:
+    static void fail(boost::format failureMessage) {
+        TestReporter::fail(failureMessage.str());
+    }
+
+    template <typename ParameterType, typename... ParameterTypes>
+    static void fail(boost::format failureMessage,
+            const ParameterType& messageParameter,
+            const ParameterTypes&... messageParameters) {
+        fail(failureMessage % format(messageParameter), messageParameters...);
+    }
+
 private:
     AssertionTester() = delete;
 };
