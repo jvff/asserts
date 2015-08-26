0336e81497b3767adbeb3f144142bd1369120976
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-08-26 13:32:55 -0300

    Implement AssertionsSpecificFor constructors
    - Add default constructor
    - Add constructor with subject parameter

--- include/main/cpp/AssertionsSpecificFor.hpp
+++ include/main/cpp/AssertionsSpecificFor.hpp
@@ -6,12 +6,27 @@
 
 template <typename T>
 class AssertionsSpecificFor : public CommonAssertions<T> {
+public:
+    AssertionsSpecificFor() {
+    }
+
+    AssertionsSpecificFor(const T&) {
+    }
 };
 
 template <typename ClassType, typename ReturnType, typename... ParameterTypes>
 class AssertionsSpecificFor<ReturnType(ClassType::*)(ParameterTypes...)> :
         public CommonAssertions<ReturnType(ClassType::*)(ParameterTypes...)> {
+private:
+    using SubjectType = ReturnType (ClassType::*)(ParameterTypes...);
+
 public:
+    AssertionsSpecificFor() {
+    }
+
+    AssertionsSpecificFor(const SubjectType&) {
+    }
+
     ASSERTION(isConstMethod, false)
     ASSERTION(isMethod, true)
 };
@@ -20,7 +35,16 @@ template <typename ClassType, typename ReturnType, typename... ParameterTypes>
 class AssertionsSpecificFor<ReturnType(ClassType::*)(ParameterTypes...) const> :
         public CommonAssertions<
                 ReturnType(ClassType::*)(ParameterTypes...) const> {
+private:
+    using SubjectType = ReturnType (ClassType::*)(ParameterTypes...) const;
+
 public:
+    AssertionsSpecificFor() {
+    }
+
+    AssertionsSpecificFor(const SubjectType&) {
+    }
+
     ASSERTION(isConstMethod, true)
     ASSERTION(isMethod, true)
 };
