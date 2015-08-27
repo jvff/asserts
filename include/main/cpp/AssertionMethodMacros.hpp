82c46af69ec97ad048e74fe28e9c10589dc587d5
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-08-27 10:46:52 -0300

    Remove unnecessary line breaks

--- include/main/cpp/AssertionMethodMacros.hpp
+++ include/main/cpp/AssertionMethodMacros.hpp
@@ -32,14 +32,12 @@
 #define ASSERTION_WITH_PARAM(methodName, testCondition, ...) \
     template <typename T2> \
     ASSERTION_BODY_WITH_PARAMS_AND_MESSAGE(methodName, testCondition, \
-            (const T2& parameter), \
-            FAILURE_MESSAGE_FOR(methodName), __VA_ARGS__)
+            (const T2& parameter), FAILURE_MESSAGE_FOR(methodName), __VA_ARGS__)
 
 #define ASSERTION_WITH_TYPE_PARAM(methodName, testCondition, ...) \
     template <typename TypeParam> \
     ASSERTION_BODY_WITH_PARAMS_AND_MESSAGE(methodName, testCondition, \
-            (const TypeParam&), \
-            FAILURE_MESSAGE_FOR(methodName), __VA_ARGS__)
+            (const TypeParam&), FAILURE_MESSAGE_FOR(methodName), __VA_ARGS__)
 
 #define ASSERTION_BODY_WITH_MESSAGE(methodName, testCondition, ...) \
     ASSERTION_BODY_WITH_PARAMS_AND_MESSAGE(methodName, testCondition, (), \
