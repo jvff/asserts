13b36c61eb67acdfa86539e87e5fcf414e7ea280
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2015-08-28 11:05:05 -0300

    Test isNotCopyConstructible assertion

--- src/test/cpp/CopyConstructibleTest.cpp
+++ src/test/cpp/CopyConstructibleTest.cpp
@@ -7,6 +7,13 @@ TYPE_ASSERTION_TEST(CopyConstructibleTest, isCopyConstructible) {
     AssertThat<TypeParam>::isCopyConstructible();
 }
 
+TYPE_ASSERTION_TEST(CopyConstructibleTest, isNotCopyConstructible) {
+    AssertThat<TypeParam>::isNotCopyConstructible();
+}
+
+TYPE_ASSERTION_TESTS_ARE_OPPOSITE(CopyConstructibleTest, isCopyConstructible,
+        isNotCopyConstructible);
+
 SHOULD_SUCCEED(CopyConstructibleTest, isCopyConstructible,
         ClassWithDefaultConstructors,
         ClassWithDefaultCopyConstructor,
