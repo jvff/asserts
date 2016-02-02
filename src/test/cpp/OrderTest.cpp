4185a1777e8b8f691910fda85d83a884b1e4fdde
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2016-02-02 17:14:52 -0200

    Test isGreaterThan assertion
    - Use the same values for testing as the isLessThan assertion, but
      swap the pair values in the test function

--- src/test/cpp/OrderTest.cpp
+++ src/test/cpp/OrderTest.cpp
@@ -49,3 +49,42 @@ VALUES_SHOULD_FAIL(OrderTest, isLessThan,
         std::make_tuple(200000.0, 800.0),
         std::make_tuple(std::make_pair(100, 1), std::make_pair(1, 100)),
         std::make_tuple(std::make_pair(100, 100), std::make_pair(100, 100)));
+
+VALUE_ASSERTION_TEST(OrderTest, isGreaterThan) {
+    assertThat(std::get<1>(parameter)).isGreaterThan(std::get<0>(parameter));
+
+    checkResult("Expected something greater than %s, but %s was found",
+            std::get<0>(parameter), std::get<1>(parameter));
+}
+
+VALUES_SHOULD_SUCCEED(OrderTest, isGreaterThan,
+        std::make_tuple(0, 1),
+        std::make_tuple(-1, 1),
+        std::make_tuple(-4839, -3199),
+        std::make_tuple(-4839, 3199),
+        std::make_tuple(4839, 13199),
+        std::make_tuple(-0.125f, 2000.f),
+        std::make_tuple(0.125f, 100.f),
+        std::make_tuple(-0.000008, 1000000.0),
+        std::make_tuple(800.0, 200000.0),
+        std::make_tuple(std::make_pair(1, 100), std::make_pair(100, 1)));
+VALUES_SHOULD_FAIL(OrderTest, isGreaterThan,
+        std::make_tuple(1, 0),
+        std::make_tuple(1, 1),
+        std::make_tuple(1, -1),
+        std::make_tuple(-1, -1),
+        std::make_tuple(-3199, -4839),
+        std::make_tuple(-3199, -3199),
+        std::make_tuple(3199, -4839),
+        std::make_tuple(13199, 4839),
+        std::make_tuple(13199, 13199),
+        std::make_tuple(2000.f, -0.125f),
+        std::make_tuple(2000.f, 2000.f),
+        std::make_tuple(-0.125f, -0.125f),
+        std::make_tuple(100.f, 0.125f),
+        std::make_tuple(1000000.0, -0.000008),
+        std::make_tuple(1000000.0, 1000000.0),
+        std::make_tuple(-0.000008, -0.000008),
+        std::make_tuple(200000.0, 800.0),
+        std::make_tuple(std::make_pair(100, 1), std::make_pair(1, 100)),
+        std::make_tuple(std::make_pair(100, 100), std::make_pair(100, 100)));
